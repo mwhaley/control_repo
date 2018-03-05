ea994c3d1b7ee68a0e40dce6dde3c3fb38727850
mwhaley <mwhaley@users.noreply.github.com>
2018-03-05 15:34:56 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,15 +2,14 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
-  dockeragent::node ['ssh.puppet.vm':}
-  
-  host {'weeb.puppet.vm':
+    
+  host {'web.puppet.vm':
     ensure => present,
-    ip => '172.18.0.3',
+    ip     => '172.18.0.3',
   }
   
   host {'db.puppet.vm':
     ensure => present,
-    ip => '172.18.0.2',
+    ip     => '172.18.0.2',
   }
 }
